27e7dfae26666f3091b7e16f40b09e424b232fca
Jordan Phillips <jordan.m.phillips23@gmail.com>
2018-09-17 09:02:36 -0400

    whole project

--- src/ProblemSet1.java
+++ src/ProblemSet1.java
@@ -15,13 +15,39 @@ public class ProblemSet1 {
 	
 	public static void main(String[] args) {
 		System.out.println("/// EXERCISE 1 /////////////////////////////////////////////////\n");
-		
+		System.out.println("   ##    ######  ######## ########");
+		System.out.println("  #  #   #     # #        #");
+		System.out.println(" #    #  #     # #        #");
+		System.out.println("######## ######  #        ########");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       #               #");
+		System.out.println("#      # #       ######## ########");
+
+			
 		System.out.println("/// EXERCISE 2 /////////////////////////////////////////////////\n");
+		
+		System.out.println("  /\\     /\\     /\\");
+		System.out.println(" /  \\   /  \\   /  \\");
+		System.out.println("/    \\ /    \\ /    \\");
+		System.out.println("\\    / \\    / \\    /");
+		System.out.println(" \\  /   \\  /   \\  /");
+		System.out.println("  \\/     \\/     \\/");
 		
 		System.out.println("/// EXERCISE 3 /////////////////////////////////////////////////\n");
 		
+		System.out.print("   \\\\\\\\\\ \n");
+		System.out.print("  +\"\"\"\"\"+ \n");
+		System.out.print("(|  0 0  |) \n");
+		System.out.print(" |   ^   | \n");
+		System.out.print(" |  '-'  | \n");
+		System.out.print("  +-----+ \n");
+		
 		System.out.println("/// EXERCISE 4 /////////////////////////////////////////////////\n");
 		
+		System.out.println(" H E L L O \n E       A \n L       P \n L       C \n O A P C S");
+		
 		System.out.println("/// EXERCISE 5 /////////////////////////////////////////////////\n");
+		
+		System.out.println("\"The answer's quite simple,\" the professor said. \"Just think outside the box!\"");
 	}
 }
